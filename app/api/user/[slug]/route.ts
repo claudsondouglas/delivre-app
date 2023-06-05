c88745f1b9bc38442aa3b7226c26160f778c385b
Claudson Douglas <claudsondouglas.13@gmail.com>
2023-06-04 21:24:47 -0300

    fix: remover console.log

--- app/api/user/[slug]/route.ts
+++ app/api/user/[slug]/route.ts
@@ -3,8 +3,6 @@ import users from "../userdb";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request, { params } : { params: { slug: number } }) {
-    console.log(params)
-    // find with lodash a item in user with slug = params.slug
     const array = find(users, { slug: params.slug });
     
     if (!array) {
